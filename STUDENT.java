3ccd21847d1ef51b37223bcbd0f580cfd5b74b3f
Chandana4328 <venuschandana63@gmail.com>
2024-07-30 22:28:25 +0530

    Add files via upload

---
+++ STUDENT.java
@@ -0,0 +1,30 @@
+import java.io.Serializable;
+
+public class STUDENT implements Serializable {
+    private String name;
+    private String rollNumber;
+    private String grade;
+
+    public STUDENT(String name, String rollNumber, String grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Roll Number: " + rollNumber + ", Grade: " + grade;
+    }
+}
